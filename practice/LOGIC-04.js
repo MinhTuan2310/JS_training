36ed22189a50065e5d858b9d223c419caa43466c
Tuan Le <lmtuan2310@gmail.com>
2021-08-30 16:02:26 +0700

    upload code

--- practice/LOGIC-04.js
+++ practice/LOGIC-04.js
@@ -1,37 +1,38 @@
-function statisticsNumbers(numberList) {
-  if(!Array.isArray(numberList) || numberList.length === 0) return {};
+// function statisticsNumbers(numberList) {
+//   if(!Array.isArray(numberList) || numberList.length === 0) return {};
 
-  return numberList.reduce((obj, number, index) => {
-    if(!obj[number]) {
-      obj[number] = 1;
-    } else {
-      obj[number] +=  1;
-    }
+//   return numberList.reduce((obj, number, index) => {
+//     if(!obj[number]) {
+//       obj[number] = 1;
+//     } else {
+//       obj[number] +=  1;
+//     }
 
-    return obj;
-  }, {});
-}
+//     return obj;
+//   }, {});
+// }
 
 
-function  findMostFrequentNumber(numberList) {
-  if(!Array.isArray(numberList)) return 0;
+// function  findMostFrequentNumber(numberList) {
+//   if(!Array.isArray(numberList)) return 0;
   
-  const obj = statisticsNumbers(numberList);
+//   const obj = statisticsNumbers(numberList);
 
-  const maxNumber = Object.values(obj).reduce((max, number, index) => {
-    if(max === 0 || number > max) {
-      max = number;
-    }
+//   const maxNumber = Object.values(obj).reduce((max, number, index) => {
+//     if(max === 0 || number > max) {
+//       max = number;
+//     }
 
-    return max;
-  }, 0);
+//     return max;
+//   }, 0);
 
-  return +Object.keys(obj)
-  .find(key => obj[key] === maxNumber);
+//   return +Object.keys(obj)
+//   .find(key => obj[key] === maxNumber);
+// }
 
-}
+// console.log(findMostFrequentNumber([1]));
+// console.log(findMostFrequentNumber([1, 2, 3, 2]));
+// console.log(findMostFrequentNumber([1, 2, 3, 2, 3, 3, 4, 4, 4, 4, 4]));
+// console.log(findMostFrequentNumber([1, 2, 3, 2, 4]));
 
-console.log(findMostFrequentNumber([1]));
-console.log(findMostFrequentNumber([1, 2, 3, 2]));
-console.log(findMostFrequentNumber([1, 2, 3, 2, 3, 3, 4, 4, 4, 4, 4]));
-console.log(findMostFrequentNumber([1, 2, 3, 2, 4]));
+
