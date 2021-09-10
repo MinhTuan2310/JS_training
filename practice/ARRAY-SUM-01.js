69d4e714c62c780875f211283779444231de9c21
Tuan Le <lmtuan2310@gmail.com>
2021-09-10 18:54:37 +0700

    upload code

--- practice/ARRAY-SUM-01.js
+++ practice/ARRAY-SUM-01.js
@@ -1,14 +1,26 @@
 function  sumEvenNumbers(numberList) {
-  if(!Array.isArray(numberList)) return false;
+  if(!Array.isArray(numberList) || numberList.length === 0) return false;
 
-  return numberList
-    .reduce((acc, value, index) => {
-      if(value % 2 === 0 && index !== 0 && value > numberList[index - 1]) {
-        acc = acc + value;
-      }
+  // return numberList
+  //   .reduce((acc, value, index) => {
+  //     if(value % 2 === 0 && index !== 0 && value > numberList[index - 1]) {
+  //       acc = acc + value;
+  //     }
       
-      return acc;
-    }, 0);
+  //     return acc;
+  //   }, 0);
+
+  let sum = 0;
+
+  for(let i = 1; i < numberList.length; i++) {
+    const number = numberList[i];
+
+    if(number === Math.max(number, numberList[i - 1]) && number % 2 === 0) {
+      sum += number;
+    }
+  }
+
+  return sum;
 }
 console.log(sumEvenNumbers([1,2,3,4,5,8,6]));
 console.log(sumEvenNumbers([-10, -4, 2, 8, 5]));
--- practice/ARRAY-SUM-02.js
+++ practice/ARRAY-SUM-02.js
@@ -1,12 +1,23 @@
 function sumAllDigits(numberList) {
   if (!Array.isArray(numberList)) return false;
 
-  return numberList
-  .join("")
-  .split("")
-  .reduce((acc, value) => acc + parseInt(value), 0);
+  // return numberList
+  // .join("")
+  // .split("")
+  // .reduce((sum, number) => sum + +number, 0);
+
+  let totalSum = 0;
+  numberList.forEach((number, index) => {
+    let sum =  0;
+
+    sum = number.toString().split("").reduce((a, b) => a + +b, 0);
+
+    totalSum += sum;
+  })
+
+  return totalSum;
 }
-console.log(sumAllDigits([]));
-console.log(sumAllDigits([4]));
-console.log(sumAllDigits([123, 4]));
-console.log(sumAllDigits([1234, 55]));
+// console.log(sumAllDigits([]));
+// console.log(sumAllDigits([4]));
+// console.log(sumAllDigits([123, 4]));
+console.log(sumAllDigits([12, 3]));
